f85dfd0b6c533ae2056bd00cecf1796ebe59a4aa
小林翔太郎 <sho@MBP.local>
2020-06-12 06:03:19 +0900

    end of ep3

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include('tweet.urls')),
     path('', include('user.urls')),
+    path('admin/', admin.site.urls),
 ]
--- tweet/views.py
+++ tweet/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'tweet/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
